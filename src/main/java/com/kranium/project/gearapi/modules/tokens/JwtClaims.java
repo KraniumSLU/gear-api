80364143ca57940ef3854e015e7376f5fb307c10
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-04-29 13:41:39 -0300

    create JwtClaims

---
+++ src/main/java/com/kranium/project/gearapi/modules/tokens/JwtClaims.java
@@ -0,0 +1,17 @@
+package com.kranium.project.gearapi.modules.tokens;
+
+
+import com.kranium.project.gearapi.modules.users.domain.Role;
+
+import java.time.Instant;
+
+public record JwtClaims(
+        String sub,
+        String name,
+        Role role,
+        Instant issuedAt,
+        Instant expiration
+) {
+
+
+}
