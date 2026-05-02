f3c1fbca4eea07c2870d0708b59317a31a125906
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-05-02 20:32:19 -0300

    update: Role.java v1: project:gear-api date: 02052026

--- src/main/java/com/kranium/project/gearapi/modules/users/domain/Role.java
+++ src/main/java/com/kranium/project/gearapi/modules/users/domain/Role.java
@@ -2,12 +2,8 @@ package com.kranium.project.gearapi.modules.users.domain;
 
 
 public enum Role {
-    ADMIN("ADM"),
-    USUARIO("USUARIO"),
-    MANUTENCAO("MANUTENCAO"),
-    VISITANTE("VISITANTE");
-    private final String name;
-    Role(String name){
-        this.name=name;
-    }
+        ROLE_ADMIN,
+        ROLE_USER,
+        ROLE_SUPPORT,
+        ROLE_VISITOR
 }
