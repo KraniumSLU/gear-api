8d1b57eb0f6e15a7837827cd11cb454f80700f4c
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-05-16 13:02:07 -0300

    updated: Session v1: project:gear-api date: 16052026

--- src/main/java/com/kranium/project/gearapi/modules/identity/domain/Session.java
+++ src/main/java/com/kranium/project/gearapi/modules/identity/domain/Session.java
@@ -1,4 +1,44 @@
 package com.kranium.project.gearapi.modules.identity.domain;
 
-public class Session {
+import com.kranium.project.gearapi.modules.identity.inheritanceClass.domain.Entidade;
+import com.kranium.project.gearapi.modules.users.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_session")
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class Session extends Entidade {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String ipAddress;
+
+    @Column(nullable = false)
+    private String userAgent;
+
+    @Column(nullable = false)
+    private String device;
+
+    @Column(nullable = false)
+    private Instant startedAt;
+
+    @Column
+    private Instant lastAccess;
+
+    @Column(nullable = false)
+    private boolean active = true;
 }
